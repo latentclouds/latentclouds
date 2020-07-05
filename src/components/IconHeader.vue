cb602bd90c756923639473e2b5c7bfaf3958f84b
Joel Sequeira <joen.sg@gmail.com>
2020-07-05 13:03:00 +0100

    add rectangular boxes to HomePage (#3)
    Co-authored-by: Joel Sequeira <joensg@gmail.com>

---
+++ src/components/IconHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="icon-header">
+    <p>{{ img }}</p>
+    <p class="text-uppercase">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IconHeader',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    img: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.icon-header {
+  text-align: center;
+  height: 91px;
+}
+</style>
--- src/components/routes/HomePage.vue
+++ src/components/routes/HomePage.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home-page">
     <Index msg="Let's get started!" />
+    <RectangularBoxContainer />
   </div>
 </template>
 
 <script>
 import Index from '@/components/Index.vue'
+import RectangularBoxContainer from '@/components/RectangularBoxContainer.vue'
 
 export default {
   name: 'HomePage',
   components: {
-    Index
+    Index,
+    RectangularBoxContainer
   }
 }
 </script>
