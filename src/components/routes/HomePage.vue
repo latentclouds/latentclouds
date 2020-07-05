eadef447822fd179383da2e788f23e54266ac16b
Joel Sequeira <joen.sg@gmail.com>
2020-07-05 13:04:09 +0100

    Revert "add rectangular boxes to HomePage (#3)"
    This reverts commit cb602bd90c756923639473e2b5c7bfaf3958f84b.

--- src/components/routes/HomePage.vue
+++ src/components/routes/HomePage.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home-page">
     <Index msg="Let's get started!" />
-    <RectangularBoxContainer />
   </div>
 </template>
 
 <script>
 import Index from '@/components/Index.vue'
-import RectangularBoxContainer from '@/components/RectangularBoxContainer.vue'
 
 export default {
   name: 'HomePage',
   components: {
-    Index,
-    RectangularBoxContainer
+    Index
   }
 }
 </script>
